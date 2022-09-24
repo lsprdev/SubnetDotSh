1e9210cdde863df58913ca7ce9c6c8732ba0f9b9
OGabrielPereira <ogabrielpereira@pm.me>
2022-09-24 19:13:23 -0300

    Color added

--- subnet.sh
+++ subnet.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+DARKGRAY='\033[1;30m'
+RED='\033[0;31m'    
+LIGHTRED='\033[1;31m'
+GREEN='\033[0;32m'    
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'    
+PURPLE='\033[0;35m'    
+LIGHTPURPLE='\033[1;35m'
+CYAN='\033[0;36m'    
+WHITE='\033[1;37m'
+SET='\033[0m'
 conv() {
-    echo "| ID | ENDEREÇO_DE_REDE | PRIMEIRO_HOST | ÚLTIMO_HOST | BROADCAST |"
+    echo -e "${GREEN}| ID | ENDEREÇO_DE_REDE | PRIMEIRO_HOST | ÚLTIMO_HOST | BROADCAST |${SET}"
     i=1
     while [ $i -le $subnets ];
     do  
@@ -9,7 +19,7 @@ conv() {
         first_host=$(( $last_net + 1 ))
         last_host=$(( $last_broadcast - 1 ))
 
-        echo "| ${i} | ${split[0]}.${split[1]}.${split[2]}.${last_net} | ${split[0]}.${split[1]}.${split[2]}.${first_host} | ${split[0]}.${split[1]}.${split[2]}.${last_host} | ${split[0]}.${split[1]}.${split[2]}.${last_broadcast} |";   
+        echo -e "${GREEN}|${SET} ${i} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_net} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${first_host} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_host} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_broadcast} ${GREEN}|${SET}";   
 
         # Encontrando os octetos de rede e broadcast
         last_net=$((last_net + hosts))
@@ -20,14 +30,14 @@ conv() {
 }
 
 find() {
-    echo "| ID | ENDEREÇO_DE_REDE | PRIMEIRO_HOST | ÚLTIMO_HOST | BROADCAST |"
+    echo -e "${GREEN}| ID | ENDEREÇO_DE_REDE | PRIMEIRO_HOST | ÚLTIMO_HOST | BROADCAST |${SET}"
     i=1
     while [ $i -le $subnets ];
     do
         first_host=$(( $last_net + 1 ))
         last_host=$(( $last_broadcast - 1 ))
         if [ ${split[3]} -ge $last_net ] && [ ${split[3]} -le $last_broadcast ]; then
-            echo "| ${i} | ${split[0]}.${split[1]}.${split[2]}.${last_net} | ${split[0]}.${split[1]}.${split[2]}.${first_host} | ${split[0]}.${split[1]}.${split[2]}.${last_host} | ${split[0]}.${split[1]}.${split[2]}.${last_broadcast} |";   
+            echo -e "${GREEN}|${SET} ${i} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_net} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${first_host} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_host} ${GREEN}|${SET} ${split[0]}.${split[1]}.${split[2]}.${last_broadcast} ${GREEN}|${SET}";   
         fi
 
         last_net=$((last_net + hosts))
@@ -38,33 +48,33 @@ find() {
 }
 
 clear;
-echo "----------------------------------------";
-echo "--- SubnetDotSh --- Calculadora IPv4 ---";
-echo "----- por: <ogabrielpereira@pm.me> -----";
-echo "----------------------------------------";
-echo "Digite 1 para converter de /24 para /25../32."
-echo "Digite 2 para achar apenas as informações do endereço IP informado."
+echo -e "${GREEN}----------------------------------------${SET}";
+echo -e "${GREEN}--- SubnetDotSh --- Calculadora IPv4 ---${SET}";
+echo -e "${GREEN}----- por: <ogabrielpereira@pm.me> -----${SET}";
+echo -e "${GREEN}----------------------------------------${SET}";
+echo -e "Digite 1 para converter de /24 para /25../32."
+echo -e "Digite 2 para achar apenas as informações do endereço IP informado."
 read -p "Opção(Padrão 1): " c
 if [ -z $c ]; then
-    echo "Opção padrão selecionada."
+    echo -e "${YELLOW}Opção padrão selecionada.${SET}"
 fi
 
 # IP HANDLING
 read -p "Endereço IP: " ip 
 split=(${ip//./ }) # Split the IP address into 4 octets
 if [ ${#split[*]} -lt 4 ] || [ ${#split[*]} -gt 4 ] ; then
-    echo "IP Inválido!"
+    echo -e "${RED}IP Inválido!${SET}"
     exit 1
 fi
 
 # SUBNET MASK HANDLING
 read -p "Máscara de Sub-rede(CIDR)(Padrão: 24): " mask
 if [ -z $mask ]; then
-    echo "Máscara padrão selecionada."
+    echo -e "${YELLOW}Máscara padrão selecionada.${SET}"
     mask=24
 fi
 if [ $mask -gt 32 ] || [ $mask -lt 24 ]; then
-    echo "Mascara de Sub-rede Inválida!"
+    echo -e "${RED}Mascara de Sub-rede Inválida!${SET}"
     exit 1
 fi
 
@@ -76,10 +86,10 @@ last_broadcast=$((hosts-1))
 
 # OUTPUTS
 clear;
-echo "Endereço IP: $ip"
-echo "Nova Máscara de sub-rede: 255.255.255.$((256-$hosts)) = /$mask"
-echo "Número de hosts por sub-rede: $((hosts-2))"
-echo "Número de sub-redes: $subnets"
+echo -e "${BLUE}Endereço IP:${SET} ${DARKGRAY}$ip${SET}"
+echo -e "${BLUE}Nova Máscara de sub-rede:${SET} ${DARKGRAY}255.255.255.$((256-$hosts)) = /$mask ${SET}"
+echo -e "${BLUE}Número de hosts por sub-rede:${SET} ${DARKGRAY}$((hosts-2)) ${SET}"
+echo -e "${BLUE}Número de sub-redes:${SET} ${DARKGRAY}$subnets ${SET}"
 
 case $c in
     1)
@@ -92,7 +102,7 @@ case $c in
         conv | column -t
         ;;
     *)
-        echo "Opção inválida, saindo..."
+        echo -e "${RED}Opção inválida, saindo...${SET}"
         exit 1
         ;;
 esac
